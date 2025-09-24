bd0f90c2ed2042dfa837895ceea007b773f656ea
Pamela Prados <pamgendrop@gmail.com>
2025-09-23 19:47:44 -0600

    Refactor database connection handling in prueba.py; remove obtener_tareas endpoint

--- routes/prueba.py
+++ routes/prueba.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 import os
 from dotenv import load_dotenv
 from flask_mysqldb import MySQL
-from config.db import get_db_connection
+from config.db import connection
 
 load_dotenv()
 mysql = MySQL()
@@ -20,31 +20,12 @@ def validar_campos_requeridos(data, campos):
 
 # ------ ENDPOINTS -------
 
-#creamos un endpoint para obtener tareas de un usuario en especifico
-@tareas_bp.route('/obtener', methods=['GET'])
-@jwt_required()
-def obtener_tareas():
-    current_user = get_jwt_identity()
-    cursor = get_db_connection()
-    query = '''
-               SELECT a.id_tarea, a.descripcion, b.nombre, b.email, a.creadoEn
-               FROM tareas as a 
-               INNER JOIN users as b on a.id_usuario = b.id_usuario 
-               WHERE a.id_usuario = %s;
-            '''
-    cursor.execute(query, (current_user,))
-    lista = cursor.fetchall()
-    cursor.close()
-    if not lista:
-        return jsonify({"error": "Este usuario no tiene tareas"}), 400
-    else:
-        return jsonify({"lista": lista}), 200
 
 # Endpoint para obtener todos los productos
 @productos_bp.route('/productos', methods=['GET'])
-@jwt_required()
+#@jwt_required()
 def obtener_productos():
-    cursor = get_db_connection()
+    cursor = connection()
     query = "SELECT * FROM Productos;"
     cursor.execute(query)
     productos = cursor.fetchall()
